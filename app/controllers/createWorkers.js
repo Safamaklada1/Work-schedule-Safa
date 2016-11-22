a405b788b5a7abfc9392e02c9ebc59772c32e4f4
safa maklada <safa maklada>
2016-11-22 20:44:54 +0200

    22112016

--- app/controllers/createWorkers.js
+++ app/controllers/createWorkers.js
@@ -1,32 +1,97 @@
-(function(){
-	angular.module("app").controller("createWorkers",createWorkers);
-	
-	function createWorkers($scope){
-		$scope.name;
-		$scope.shift;
-		$scope.jobTitle={
-			value1:'v',
-			value2:'v',
-			value3:'v',
-			value4:'v',
-			value5:'v',
-			value6:'v',
-			};
-		$scope.worker=function(){
-			var allworkers=(JSON.parse(localStorage.getItem("allworkers")));
-			 if(!allworkers)allworkers=[];
-			var worker1={shift:$scope.shift,name:$scope.name,a:$scope.jobTitle.value1,b:$scope.jobTitle.value2,c:$scope.jobTitle.value3,d:$scope.jobTitle.value4,e:$scope.jobTitle.value5,f:$scope.jobTitle.value6};
-			allworkers.push(worker1);
-			console.log(worker1);
-			localStorage.setItem("allworkers",JSON.stringify(allworkers));
-			
-			
+(function() {
+	angular.module("app").controller("createWorkers", createWorkers);
+
+	function createWorkers($scope) {
+		$scope.date = moment(Date.now()).format("DD/MM/YYYY");
+		$scope.name
+		$scope.shift
+		$scope.email
+		$scope.address
+		$scope.phone
+		$scope.jobTitle = {
+			value1 : 'v',
+			value2 : 'v',
+			value3 : 'v',
+			value4 : 'v',
+			value5 : 'v',
+			value6 : 'v',
 		};
-		$scope.getList=function(){
-			
-			$scope.getWorker=JSON.parse(localStorage.getItem("allworkers"));
-			
-	};
+		$scope.addSerialNumber = function() {
+			var i = 1
+			$('tbody tr').each(function(index) {
+				$(this).find('td:nth-child(1)').html(index + 1);
+			});
+		};
+		$scope.worker = function() {
+			var shift1;
+			var shift2;
+			var shift3;
+			var allworkers = (JSON.parse(localStorage.getItem("allworkers")));
+			if (!allworkers)
+				allworkers = [];
+			var worker1 = {
+				shift : $scope.shift,
+				name : $scope.name,
+				email : $scope.email,
+				address : $scope.address,
+				phone : $scope.phone,
+				shiftManager : $scope.jobTitle.value1,
+				control : $scope.jobTitle.value2,
+				kOperator : $scope.jobTitle.value3,
+				lab : $scope.jobTitle.value4,
+				k200 : $scope.jobTitle.value5,
+				pr : $scope.jobTitle.value6
+			};
+			allworkers.push(worker1);
+			// console.log(worker1);
+			if (worker1.shift == 1) {
+				shift1 = (JSON.parse(localStorage.getItem("shift1")));
+				if (!shift1)
+					shift1 = [];
+
+				shift1.push(worker1.name);
+				localStorage.setItem("shift1", JSON.stringify(shift1));
+
+			} else if (worker1.shift == 2) {
+				shift2 = (JSON.parse(localStorage.getItem("shift2")));
+				if (!shift2)
+					shift2 = [];
+
+				shift2.push(worker1.name);
+				localStorage.setItem("shift2", JSON.stringify(shift2));
+
+			} else if (worker1.shift == 3) {
+				shift3 = (JSON.parse(localStorage.getItem("shift3")));
+				if (!shift3)
+					shift3 = [];
+
+				shift3.push(worker1.name);
+				localStorage.setItem("shift3", JSON.stringify(shift3));
+
+			}
+			$scope.getWorker = allworkers;
+			localStorage.setItem("allworkers", JSON.stringify(allworkers));
+
+		};
+		$scope.getList = function() {
+
+			$scope.getWorker = JSON.parse(localStorage.getItem("allworkers"));
+			// $scope.getshift=JSON.parse(localStorage.getItem("shift1"));
+		};
+		$scope.deleteWorker = function(index) {
+
+			var deletWorker = $scope.getWorker.splice(index, 1);
+			localStorage.setItem("allworkers", JSON.stringify($scope.getWorker));
+			console.log(deletWorker);
+
+			if (deletWorker[0].shift === 1) {
+				var shift = JSON.parse(localStorage.getItem("shift1"));
+				var index = shift.indexOf(deletWorker[0].name);
+				shift.splice(index, 1);
+				localStorage.setItem("shift1", JSON.stringify(shift));
+			}
+
+		};
 	}
-	
+
 })();
